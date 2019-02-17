7b9f736676dfe40a7e4bb281b4419b44424d4e64
Nithesh Koneswaran <Nithesh@hotmail.co.uk>
2019-02-17 12:28:18 +0000

    Added code within the methods of the controller that will parse the JSON objects, which can then be accessed in the view to be presented nicely. To Do Next: Allow the user to move from one article to another, will be implemented in the header

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,30 @@
+require 'json'
 class ArticlesController < ApplicationController
+  
+  
   def article1
+    file = File.read('app/assets/javascripts/article-1.json')
+    @article1 = JSON.parse(file)
+
   end
 
   def article2
+    file1 = File.read('app/assets/javascripts/article-2.json')
+    @article2 = JSON.parse(file1)
   end
 
   def article3
+    file2 = File.read('app/assets/javascripts/article-3.json')
+    @article3 = JSON.parse(file2)
   end
 
   def article4
+    file3 = File.read('app/assets/javascripts/article-4.json')
+    @article4 = JSON.parse(file3)
   end
 
   def ariticle5
+    file4 = File.read('app/assets/javascripts/article-5.json')
+    @article5 = JSON.parse(file4)
   end
 end
---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,25 @@
+class HomeController < ApplicationController
+  def home
+  end
+
+  def article1
+
+  end
+
+  def article2
+
+  end
+
+  def article3
+
+  end
+
+  def article4
+
+  end
+
+  def article5
+  
+  end
+
+end
