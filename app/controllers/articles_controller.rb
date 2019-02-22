c2657186d415963ac603b5faba786fdd81ff910f
Nithesh Koneswaran <Nithesh@hotmail.co.uk>
2019-02-22 19:19:52 +0000

    Fixed some css where the body was not dynamic with the content, this was fixed by setting the background-size of the body and html as 100%

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,9 @@
 require 'json'
 class ArticlesController < ApplicationController
-  
-  
   def article1
     file = File.read('app/assets/javascripts/article-1.json')
     @article = JSON.parse(file)
-  
+    @url = @article["body"][2]["model"]["url"]
     @altText = @article["body"][2]["model"]["altText"]
     if @url.to_s.empty?
       @url = "/assets/EarthLogo.png"
@@ -18,22 +16,61 @@ class ArticlesController < ApplicationController
   end
 
   def article2
-    file1 = File.read('app/assets/javascripts/article-2.json')
-    @article = JSON.parse(file1)
+    file = File.read('app/assets/javascripts/article-2.json')
+    @article = JSON.parse(file)
+    
+    @url1 = @article["body"][1]["model"]["url"]
+    @altText1 = @article["body"][1]["model"]["altText"]
+    
+    @url2 = @article["body"][4]["model"]["url"]
+    @altText2 = @article["body"][4]["model"]["altText"]
+
+    @url3 = @article["body"][6]["model"]["url"]
+    @altText3 = @article["body"][6]["model"]["altText"]
+
+    if @url.to_s.empty?
+      @url = "/assets/EarthLogo.png"
+    end
+
+    if @altText.to_s.empty?
+      @altText = "Image Not Found"
+    end
   end
 
   def article3
-    file2 = File.read('app/assets/javascripts/article-3.json')
-    @article = JSON.parse(file2)
+    file = File.read('app/assets/javascripts/article-3.json')
+    @article = JSON.parse(file)
   end
 
   def article4
-    file3 = File.read('app/assets/javascripts/article-4.json')
-    @article = JSON.parse(file3)
+    file = File.read('app/assets/javascripts/article-4.json')
+    @article = JSON.parse(file)
   end
 
   def article5
-    file4 = File.read('app/assets/javascripts/article-5.json')
-    @article = JSON.parse(file4)
+    file = File.read('app/assets/javascripts/article-5.json')
+    @article = JSON.parse(file)
+
+     @url1 = @article["body"][1]["model"]["url"]
+    @altText1 = @article["body"][1]["model"]["altText"]
+    
+    @url2 = @article["body"][2]["model"]["url"]
+    @altText2 = @article["body"][2]["model"]["altText"]
+
+
+    if @url.to_s.empty?
+      @url = "/assets/EarthLogo.png"
+    end
+
+    if @altText.to_s.empty?
+      @altText = "Image Not Found"
+    end
+
   end
+
+  def ranks 
+
+  end
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'articles/article1'
-  get 'articles/article2'
-  get 'articles/article3'
-  get 'articles/article4'
-  get 'articles/article5'
+  get '/article#1', to:'articles#article1', as: 'article1'
+  get '/article#2', to:'articles#article2', as: 'article2'
+  get '/article#3', to:'articles#article3', as: 'article3'
+  get '/article#4', to:'articles#article4', as: 'article4'
+  get '/article#5', to:'articles#article5', as: 'article5'
   root 'articles#article1'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
