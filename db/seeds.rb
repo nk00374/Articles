b97f2b9055be1957404c93e4b52eac9533b4dbe9
Nithesh Koneswaran <Nithesh@hotmail.co.uk>
2019-02-22 20:51:11 +0000

    Added seeds which will help us rank the 5 articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+#Deletes any existing records
+Ranking.delete_all
+
+#Repopulates the database
+Ranking.create(name:"Article1",
+			   rank: 0,
+			   read: false)
+
+Ranking.create(name:"Article2",
+			   rank: 0,
+			   read: false)
+
+Ranking.create(name:"Article3",
+			   rank: 0,
+			   read: false)
+
+Ranking.create(name:"Article4",
+			   rank: 0,
+			   read: false)
+
+Ranking.create(name:"Article5",
+			   rank: 0,
+			   read: false)
+
